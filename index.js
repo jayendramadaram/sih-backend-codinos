4cb5c39458ab0838398923ab7d1202ebb5991dec
Srikhar Shashi D <dasojusrikhar@gmail.com>
2022-08-04 15:37:29 +0530

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app=express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//Route imports
+const authRoute = require('./routes/auth');
+// const postRoute= require('./routes/posts');
+
+dotenv.config();
+
+//mongo connect
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},()=>{console.log("Mongo Connection Success");});
+
+
+
+//global middlewares
+
+app.use(express.json());
+
+//route middleware
+app.use('/auth',authRoute);
+
+
+
+
+
+
+
+app.listen(6905,()=>{console.log("The server is listening on 6905");});
+
